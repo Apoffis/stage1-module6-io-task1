eb022a8fec76eadbef0cff3e13cb765184824432
Apoffis <abc22_08@mail.ru>
2022-10-26 17:08:50 +0400

    module6 task 1 commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static void main(String[] args) {
+
+        File file = new File("src/main/resources/Profile.txt");
+        System.out.println(getDataFromFile(file));
+
+    }
+
+    public static Profile getDataFromFile(File file) {
+
+        String line;
+        Map<String, String> profile = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            while ((line = br.readLine()) != null) {
+                String[] keyValue = line.split(": ");
+                profile.put(keyValue[0], keyValue[1]);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(profile.get("Name"), Integer.parseInt(profile.get("Age")), profile.get("Email"), Long.parseLong(profile.get("Phone")));
     }
 }
